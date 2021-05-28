10f2017534d09ffb8d1016da9f9a94216452528c
Glenn Barr <glenn.barr@focusrite.com>
2021-05-28 11:35:06 +0100

    Amend header and JS

--- themes/meghna-hugo/assets/js/script.js
+++ themes/meghna-hugo/assets/js/script.js
@@ -87,7 +87,7 @@ jQuery(function ($) {
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				html_body.animate({
-					scrollTop: target.offset().top - 175
+					scrollTop: target.offset().top - 50
 				}, 1500, 'easeInOutExpo');
 				$('.navbar-toggler').click();
 				return false;
